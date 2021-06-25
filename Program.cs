d0fcaa0fc5c25589e1459d9bbf4c7074ae684594
Artem <a.khimin@yandex.ru>
2021-06-26 01:15:55 +0300

    Доделал

--- Program.cs
+++ Program.cs
@@ -4,9 +4,66 @@ namespace GB_02_01
 {
     class Program
     {
+        static int num;
+        static string q;
+        static void PrimeNumber()
+        {
+            do
+            {
+            NewCycle:
+                int d = 0;
+                int i = 2;
+
+                do
+                {
+                    Console.WriteLine("Введите число на проверку, простое оно или составное: ");
+                    try
+                    {
+                        num = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Введите число!");
+                        goto NewCycle;
+                    }
+                    if (num <= 0)
+                        Console.WriteLine("Введите положительное число!");
+                } while (num <= 0);
+
+                while (i < num)
+                {
+                    if (num % i == 0)
+                    {
+                        d++;
+                    }
+                    i++;
+                }
+
+                if (d == 0)
+                    Console.WriteLine($"{num} -число простое ");
+                else
+                    Console.WriteLine($"{num} -число составное ");
+
+                Console.WriteLine("Нажмите на 'q' для выхода из цикла:");
+
+                q = Console.ReadLine();
+            } while (q != "q");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrimeNumber();
         }
+        //  Тест
+        //  На вход подаем 71, ожидаем простое
+        //  На вход подаем 7, ожидаем простое
+        //  На вход подаем 19, ожидаем простое
+        //  На вход подаем 4, ожидаем составное
+        //  На вход подаем 81, ожидаем составное
+
+
+
+
     }
 }
